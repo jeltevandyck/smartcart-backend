345de0618484825828b58554612e6b92b842160c
Jelte <jeltevandyck@hotmail.com>
2023-06-08 21:21:59 +0100

    Update register cart DTO

--- EPS.Smartcart.DTO/Cart/RegisterCartDTO.cs
+++ EPS.Smartcart.DTO/Cart/RegisterCartDTO.cs
@@ -3,7 +3,7 @@
 public class RegisterCartDTO
 {
     public string Id { get; set; }
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
     public string StoreId { get; set; }
     public string Code { get; set; }
 }                                      
