c4f609c9e239f0bddb71a96277e56c9200261524
Jelte <jeltevandyck@hotmail.com>
2023-06-09 16:53:38 +0100

    Update GetProductByBarcode validation

--- EPS.Smartcart.Application/Validation/Product/GetProductByBarcodeValidation.cs
+++ EPS.Smartcart.Application/Validation/Product/GetProductByBarcodeValidation.cs
@@ -17,5 +17,25 @@ public class GetProductByBarcodeValidation : AbstractValidationHandler<GetProduc
             .NotEmpty()
             .NotNull()
             .WithMessage("StoreId is required!");
+
+        RuleFor(x => x.GetProductByBarcodeDTO.StoreId)
+            .MustAsync(async (storeId, cancellationToken) =>
+            {
+                var store = await _uow.StoreRepository.GetById(storeId);
+                return store != null;
+            })
+            .WithMessage("StoreId is invalid!");
+        
+        RuleFor(x => x.GetProductByBarcodeDTO.Barcode)
+            .MustAsync(async (barcode, cancellationToken) =>
+            {
+                var products = await _uow.ProductRepository
+                    .Query(
+                        x => x.Barcode.Equals(barcode) 
+                             && x.StoreId.Equals(x.StoreId));
+                
+                return products.Count == 1;
+            })
+            .WithMessage("Product not found!");
     }
 }
